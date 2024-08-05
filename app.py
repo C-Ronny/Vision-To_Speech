f9492a37a3b192f29bd49d9fd3f731a8cdbce680
C-Ronny <138708517+C-Ronny@users.noreply.github.com>
2024-08-05 21:22:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -108,10 +108,9 @@ if uploaded_file is not None:
                 st.write(f"Translated Description ({target_language}):", translated_description)
 
                 audio_bytes = text_to_speech(translated_description, lang=target_language)
-                if audio_bytes:
-                    st.audio(audio_bytes.getvalue(), format="audio/mp3")
-                else:
-                    st.error("Error: Unable to generate audio.")
+                st.audio(audio_bytes.getvalue(), format="audio/mp3")
+
+
 else:
     st.info("Please upload a video file to start.")
 
